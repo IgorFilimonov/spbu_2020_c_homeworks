8766dd9bb28942536abef808b4930a0c7b1c9506
IgorFilimonov <st087840@student.spbu.ru>
2020-09-30 20:05:45 +0500

    i++ -> ++i

--- homework2/task5.c
+++ homework2/task5.c
@@ -10,7 +10,7 @@ int factorialRecursive(int number)
 int factorialIterative(int number)
 {
     int factorial = 1;
-    for (int i = 2; i <= number; i++)
+    for (int i = 2; i <= number; ++i)
         factorial *= i;
     return factorial;
 }

4b10feae02c0c3cb98c9c7cc578abcd147cfbb74
IgorFilimonov <st087840@student.spbu.ru>
2020-09-30 19:55:29 +0500

    many small changes

--- homework2/task4.c
+++ homework2/task4.c
@@ -1,16 +1,16 @@
+#include "../library/commonUtils/numericOperations.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "../library/commonUtils/numericOperations.h"
 
 int* convertToArrayOfDigits(int number, int* numberOfDigits)
 {
     int copyOfNumber = number;
     while (copyOfNumber != 0) {
-        (*numberOfDigits)++;
+        ++(*numberOfDigits);
         copyOfNumber /= 10;
     }
     int* digits = (int*)calloc(*numberOfDigits, sizeof(int));
-    for (int i = *numberOfDigits - 1; i >= 0; i--) {
+    for (int i = *numberOfDigits - 1; i >= 0; --i) {
         digits[i] = number % 10;
         number /= 10;
     }
@@ -19,9 +19,9 @@ int* convertToArrayOfDigits(int number, int* numberOfDigits)
 
 void bubbleSort(int* array, int sizeOfArray)
 {
-    for (int i = sizeOfArray - 1; i > 0; i--) {
+    for (int i = sizeOfArray - 1; i > 0; --i) {
         int indexOfMaximum = 0;
-        for (int j = 1; j <= i; j++) {
+        for (int j = 1; j <= i; ++j) {
             if (array[j] > array[indexOfMaximum])
                 indexOfMaximum = j;
         }
@@ -38,14 +38,14 @@ int main()
     int numberOfDigits = 0;
     int* digits = convertToArrayOfDigits(number, &numberOfDigits);
     bubbleSort(digits, numberOfDigits);
-    for (int i = 0; i < numberOfDigits; i++) {
+    for (int i = 0; i < numberOfDigits; ++i) {
         if (digits[i]) {
             swap(&digits[0], &digits[i]);
             break;
         }
     }
 
-    for (int i = 0; i < numberOfDigits; i++)
+    for (int i = 0; i < numberOfDigits; ++i)
         printf("%d", digits[i]);
     free(digits);
     return 0;

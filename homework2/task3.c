3fb90213c075381707e4596b1ca1e85691c53207
IgorFilimonov <st087840@student.spbu.ru>
2020-09-30 18:27:10 +0500

    added solution

---
+++ homework2/task3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../library/commonUtils/numericOperations.h"
+
+int main()
+{
+    int n = 0;
+    printf("Enter n:\n");
+    scanf("%d", &n);
+    int* array = (int*)calloc(n, sizeof(int));
+    printf("Enter an array:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &array[i]);
+
+    int indexOfNextZero = n - 1;
+    for (int i = 0; i < indexOfNextZero;) {
+        if (array[i] != 0) {
+            i++;
+            continue;
+        }
+        for (int j = i; j < indexOfNextZero; j++)
+           swap(&array[j], &array[j + 1]);
+        indexOfNextZero--;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", array[i]);
+    free(array);
+    return 0;
+}

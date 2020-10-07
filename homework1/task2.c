8d4f551e17edd5a6f7123f443088e55538eae435
IgorFilimonov <st087840@student.spbu.ru>
2020-10-07 21:46:09 +0500

    the solution of the second problem was changed

--- homework1/task2.c
+++ homework1/task2.c
@@ -2,36 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void reverseAnArray(int* array, int numberOfElements)
+{
+    for (int i = 0; i < numberOfElements / 2; ++i)
+        swap(&array[i], &array[numberOfElements - 1 - i]);
+}
+
 int main()
 {
     int m = 0, n = 0;
-    printf("Enter n and m:\n");
+    printf("Enter m and n:\n");
     scanf("%d%d", &m, &n);
     int* array = (int*)calloc((m + n), sizeof(int));
     printf("Enter an array:\n");
     for (int i = 0; i < m + n; ++i)
         scanf("%d", &array[i]);
 
-    int firstSegment = m, secondSegment = n, startIndex = 0;
-    while (firstSegment != secondSegment) {
-        if (firstSegment < secondSegment) {
-            for (int i = 0; i < firstSegment; ++i)
-                swap(&array[startIndex + i], &array[startIndex + secondSegment + i]);
-            secondSegment -= firstSegment;
-        }
+    reverseAnArray(array, m);
+    reverseAnArray(&array[m], n);
+    reverseAnArray(array, m + n);
 
-        if (firstSegment > secondSegment) {
-            for (int i = 0; i < secondSegment; ++i)
-                swap(&array[startIndex + i], &array[startIndex + firstSegment + i]);
-            startIndex += secondSegment;
-            firstSegment -= secondSegment;
-        }
-    }
-    for (int i = 0; i < firstSegment; ++i)
-        swap(&array[startIndex + i], &array[startIndex + secondSegment + i]);
-
+    printf("Here is a result:\n");
     for (int i = 0; i < m + n; ++i)
         printf("%d ", array[i]);
+
     free(array);
     return 0;
 }

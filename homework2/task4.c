0600b1e905e164666249dcb311d33570d0fa2da4
IgorFilimonov <st087840@student.spbu.ru>
2020-10-14 12:41:28 +0500

    fixed requested changes

--- homework2/task4.c
+++ homework2/task4.c
@@ -2,15 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* convertToArrayOfDigits(int number, int* numberOfDigits)
+int* convertToArrayOfDigits(long long number, int numberOfDigits)
 {
-    int copyOfNumber = number;
-    while (copyOfNumber != 0) {
-        ++(*numberOfDigits);
-        copyOfNumber /= 10;
-    }
-    int* digits = (int*)calloc(*numberOfDigits, sizeof(int));
-    for (int i = *numberOfDigits - 1; i >= 0; --i) {
+    int* digits = (int*)calloc(numberOfDigits, sizeof(int));
+    for (int i = numberOfDigits - 1; i >= 0; --i) {
         digits[i] = number % 10;
         number /= 10;
     }
@@ -22,24 +17,43 @@ int compare(const void* a, const void* b)
     return *((int*)a) - *((int*)b);
 }
 
-int main()
+int countNumberOfDigits(long long number)
 {
-    int number = 0;
-    printf("Enter the number:\n");
-    scanf("%d", &number);
-
     int numberOfDigits = 0;
-    int* digits = convertToArrayOfDigits(number, &numberOfDigits);
-    qsort(digits, numberOfDigits, sizeof(int), compare);
+    while (number != 0) {
+        ++numberOfDigits;
+        number /= 10;
+    }
+    return numberOfDigits;
+}
+
+void makeNumberNotStartingAtZero(int* digits, int numberOfDigits)
+{
     for (int i = 0; i < numberOfDigits; ++i) {
         if (digits[i] != 0) {
             swap(&digits[0], &digits[i]);
-            break;
+            return;
         }
     }
+}
+
+int main()
+{
+    long long number = 0;
+    printf("Enter a natural number:\n");
+    scanf("%lld", &number);
+    if (number <= 0) {
+        printf("Number isn't natural.");
+        return 0;
+    }
+
+    int sizeOfNumber = countNumberOfDigits(number);
+    int* digits = convertToArrayOfDigits(number, sizeOfNumber);
+    qsort(digits, sizeOfNumber, sizeof(int), compare);
+    makeNumberNotStartingAtZero(digits, sizeOfNumber);
 
     printf("Here is the required number:\n");
-    for (int i = 0; i < numberOfDigits; ++i)
+    for (int i = 0; i < sizeOfNumber; ++i)
         printf("%d", digits[i]);
 
     free(digits);
